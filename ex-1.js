9bedede4281dca8da0f315f27ba72f3efae23347
PNKim <paljapon.nait@gmail.com>
2024-07-17 12:42:34 +0700

    feat: resolve ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,18 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  return students.indexOf(searchStudent);
 }
 
+const students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+const searchStudent = "John";
+console.log(findStudentIndex(students, searchStudent));
 // ตอบคำถามตรงนี้จ้า
